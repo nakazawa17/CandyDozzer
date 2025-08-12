c47cc9cb2ffe9cdae936670fcbcb4c1a81f46ae4
nakazawa <nakazawakaname6@gmail.com>
2025-08-12 14:13:25 +0900

    キャンディー設置

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPosition;
+
+    public float amplitude;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // サイン(時間経過で円となり、サインカーブ（等速の波の動き）を作る）　* 動くスピード
+        // 　* amplitude(増幅度：1 ~ -1までしか返せないサインに変位量をかける)
+        float z = amplitude * Mathf.Sin(Time.time * speed);
+
+        transform.localPosition = startPosition + new Vector3(0, 0, z);
+    }
+}
